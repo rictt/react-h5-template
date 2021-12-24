b18fae51fa632dd76c51f547d4e6c6e683c00593
joey <2390923149@qq.com>
2021-12-24 20:56:24 +0800

    feat: scss pxtorem

---
+++ src/app.style.js
@@ -0,0 +1,7 @@
+import styled from 'styled-components'
+import { px2rem } from './utils/style-help'
+
+export const Title = styled.div`
+  color: skyblue;
+  font-size: ${px2rem(36)};
+`
---
+++ src/utils/style-help.js
@@ -0,0 +1,5 @@
+export const px2rem = (value) => {
+  const base = 75
+
+  return value / base + 'rem'
+}
